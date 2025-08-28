4ad1fa2e16cd395acf9482a36ca2bc48942fac9a
linqiye <linqiye0208@gmail.com>
2025-08-28 14:29:32 +0800

    update wss

--- app/common.php
+++ app/common.php
@@ -47,7 +47,7 @@ if (!function_exists('getWorkerManUrl')) {
      */
     function getWorkerManUrl()
     {
-        $ws = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'wss://' : 'ws://';
+        $ws = 'wss://';
         $host = $_SERVER['HTTP_HOST'];
         $data['admin'] = $ws . $host . '/notice';
         $data['chat'] = $ws . $host . '/msg';
